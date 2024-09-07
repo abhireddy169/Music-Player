8612d83d55b88300c71ccedbebf0f54f7c1894bb
Abhinandan Reddy <abhinandanreddy@Abhinandans-MacBook-Pro.local>
2024-09-07 20:55:33 +0530

    First commit

---
+++ src/music.js
@@ -0,0 +1,46 @@
+let allMusic = 
+[
+    {
+        name: "1975",
+        artist: "Abhinandan",
+        img: "1975",
+        src: "1975"
+    },
+
+    {
+        name: "8000 Miles",
+        artist: "Abhinandan",
+        img: "8000 miles",
+        src: "8000 miles"
+    },
+
+    {
+        name: "All Day",
+        artist: "Abhinandan",
+        img: "all day",
+        src: "all day"
+    },
+
+    {
+        name: "Kahaani",
+        artist: "Abhinandan",
+        img: "kahaani",
+        src: "kahaani"
+    },
+
+    {
+        name: "Say You Love Me",
+        artist: "Abhinandan",
+        img: "say you love me",
+        src: "say you love me"
+    },
+
+    {
+        name: "Zombie",
+        artist: "Abhinandan",
+        img: "zombie",
+        src: "zombie"
+    },
+
+   
+]
